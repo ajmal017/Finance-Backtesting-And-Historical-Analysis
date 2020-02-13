fefa138943aabc0cb42219575be312efc30e6bda
mtlberri <joffrey.armellini@gmail.com>
2020-02-13 14:31:40 -0500

    Data preparation functional (date, and beginning of prices as float)

--- app.py
+++ app.py
@@ -1,22 +1,69 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import re
+import datetime
 import seaborn as sns
 # Setting Seaborn style via set()
 sns.set()
+
+# Global Parameters #####################################################
+# Names of the files to import
+data_files_names = ['HistoricalQuotes_SPY_5Y_02112020.csv', 'HistoricalQuotes_TSLA_5Y_02112020.csv']
+columns_being_prices = ['Close']
 
 # Read the CSV file #######################################################
 def read_nasdaq_csv(file_name):
     df0 = pd.read_csv(file_name)
     # Re-order from oldest to newest, and re-indexing
     df0 = df0.sort_index(ascending=False)
-    df0 = df0.reset_index(drop=True)
+    df0 = df0.set_index('Date')
     # Rename the Close/Last column to Close
     df0 = df0.rename(columns={'Close/Last': 'Close'})
+    # Apply the function on the index to convert the string date into a proper date
+    df0.index = df0.index.map(dstring_to_date_NASDAQ)
+    # Apply the function on the price columns to convert the string price into a proper float price
+    df0['Close'] = df0['Close'].map(prince_as_float_NASDAQ)
     # Take a sub-set of the data and make it clear it is a copy
     df1 = df0.tail(10).copy()
     return df1
 ############################################################################
+
+# TRANSFORM DATE STRING INTO DATE ##########################################
+def dstring_to_date_NASDAQ(dstr):
+    ''' Method that returns a date based on the date string from the NASDAQ export '''
+    # Find the positions of / in the string
+    separator_positions = [m.start() for m in re.finditer('/', dstr)]
+    # Make sure the month is zero-padded
+    if separator_positions[0] == 1:
+        # Add a '0' so that the month string be zero-padded
+        dstr = '0' + dstr
+        # Recalculate new separator positions after dstr altered
+        separator_positions = [m.start() for m in re.finditer('/', dstr)]
+    # Make sure the day is zero-padded
+    if separator_positions[1] - separator_positions[0] == 2:
+        # Add a '0' so that the day string be zero-padded
+        dstr = dstr[:separator_positions[0] + 1] + '0' + dstr[separator_positions[0] + 1:]
+    # Transform the formatted string into date
+    format_of_date_string = '%m/%d/%Y'
+    date = datetime.datetime.strptime(dstr, format_of_date_string)
+    return date
+#########################################################################
+
+# TRANSFORM PRICE STRING INTO FLOAT ##########################################
+def prince_as_float_NASDAQ(price):
+    ''' Method that makes sure price value is a float '''
+    # Removes the $ sign from the string
+    if type(price) == str:
+        # Transform the string and return the corresponding float
+        price = price.lstrip('$')
+        return float(price)
+    elif type(price) == float:
+        # Price already a float, return it
+        return price
+    else:
+        print('Weird... price columns are neither string nor float... check source')
+###########################################################################
 
 # Calculate Change from Day to Day ########################################
 def add_change_column(df0):
@@ -36,27 +83,35 @@ def add_change_column(df0):
 ##########################################################
 
 # PLOT ############################################
-def plot_my_df(df0):
+def plot_my_df(df):
     # Plot on ax1
-    ax1 = sns.lineplot(x='Date', y='Close', data=df0)
+    ax1 = sns.lineplot(x=df.index, y='Close', data=df, markers=True)
     # Creating a new axis sharing the same x
     ax2 = ax1.twinx()
     # Creating a Custom Palette
     custom_palette = []
-    for change in df0['Change']:
+    for change in df['Change']:
         if change <= 0:
             custom_palette.append('r')
         else:
             custom_palette.append('g')
     # Plot on ax2
-    sns.barplot(x='Date', y='Change', data=df0, ax=ax2, palette=custom_palette)
+    sns.barplot(x=df.index, y='Change', data=df, ax=ax2, palette=custom_palette)
     # Redefine y axis for ax2
     ax2.set(ylim=(-3, 3))
     # Hide the grid on ax2
     ax2.grid(False)
+    plt.show()
 ##############################################################
 
 # MAIN #######################################################
+# Read data
 spy_df = read_nasdaq_csv('HistoricalQuotes_SPY_5Y_02112020.csv')
-df = add_change_column(df)
-plot_my_df(df)
+tsla_df = read_nasdaq_csv('HistoricalQuotes_TSLA_5Y_02112020.csv')
+# Add change columns
+spy_df = add_change_column(spy_df)
+#tsla_df = add_change_column(tsla_df)
+# Join dataframes into an overall dataframe
+#overall_df = spy_df.join(tsla.df, on='Date')
+
+#plot_my_df(spy_df)
