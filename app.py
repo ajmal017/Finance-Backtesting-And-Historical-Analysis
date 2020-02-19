c156d5319df000aff8b9d4654cd2f99d8e425975
mtlberri <joffrey.armellini@gmail.com>
2020-02-19 15:53:58 -0500

    Restructuring code to make it flexible to several input files (step 0)

--- app.py
+++ app.py
@@ -7,89 +7,60 @@ import matplotlib.pyplot as plt
 # Close all figure windows
 plt.close('all')
 
-# Global Parameters #####################################################
-# Names of the files to import
-data_files_names = ['HistoricalQuotes_SPY_5Y_02112020.csv', 'HistoricalQuotes_TSLA_5Y_02112020.csv']
-# List of columns from data source being days
-columns_being_prices = ['Close', 'Open', 'High', 'Low']
-# Time horizon (in open trading days)
-time_horizon = 200
+# Global Parameters ####################################################################
+# Names of the files to import (Yahoo Finance csv files)
+data_files_names = ['SPY.csv',
+                    'TSLA.csv'
+                    ]
+
+# List of columns from data source being days 
+columns_being_prices = ['Open', 'High', 'Low', 'Close']
+# Time horizon used (in open trading days)
+time_horizon = 10
 # Setting what is considered to be a SPY large move in one day = -0.5%
 spy_large_move = -0.5
 # Setting the target profit for TSLA when taking a position = + 5%
 target_profit_taking = 5.0
 # Starting capital to invest: $10,000
 starting_capital = 10000.0
+#####################################################################################
 
 # Read the CSV file  & Prepare data #######################################################
-def read_nasdaq_csv(file_name):
+def read_data_source_csv(file_name):
     df0 = pd.read_csv(file_name)
-    # Re-order from oldest to newest, and re-indexing
-    df0 = df0.sort_index(ascending=False)
+    # Set the date as the index
     df0 = df0.set_index('Date')
-    # Rename the Close/Last column to Close
-    df0 = df0.rename(columns={'Close/Last': 'Close'})
     # Apply the function on the index to convert the string date into a proper date
-    df0.index = df0.index.map(dstring_to_date_NASDAQ)
-    # Apply the function on the price columns to make sure prices are float
-    df0[columns_being_prices] = df0[columns_being_prices].applymap(price_as_float_NASDAQ)
-    # Add a column named 'Change' giving the percentage of change on Close price compared to previous day Close
-    df0 = add_change_column(df0)
+    df0.index = df0.index.map(dstring_to_date)
+    # Add a column named 'Change' giving the percentage of change between today's Close and previous Close
+    df0 = generate_change_column(df0)
     # Take a sub-set of the data (for the time horizon) and make it clear it is a copy
     df1 = df0.tail(time_horizon).copy()
     return df1
 ############################################################################
 
 # TRANSFORM DATE STRING INTO DATE ##########################################
-def dstring_to_date_NASDAQ(dstr):
-    ''' Method that returns a date based on the date string from the NASDAQ export '''
-    # Find the positions of / in the string
-    separator_positions = [m.start() for m in re.finditer('/', dstr)]
-    # Make sure the month is zero-padded
-    if separator_positions[0] == 1:
-        # Add a '0' so that the month string be zero-padded
-        dstr = '0' + dstr
-        # Recalculate new separator positions after dstr altered
-        separator_positions = [m.start() for m in re.finditer('/', dstr)]
-    # Make sure the day is zero-padded
-    if separator_positions[1] - separator_positions[0] == 2:
-        # Add a '0' so that the day string be zero-padded
-        dstr = dstr[:separator_positions[0] + 1] + '0' + dstr[separator_positions[0] + 1:]
-    # Transform the formatted string into date
-    format_of_date_string = '%m/%d/%Y'
-    date = datetime.datetime.strptime(dstr, format_of_date_string)
-    return date
+def dstring_to_date(dstr):
+    ''' Method that returns a date based on the date string  '''
+    # Set the format as it is originally (as a string in the Data Frame read from the CSV)
+    format_of_date_string = '%Y-%m-%d'
+    # Convert into a datetime object and return it
+    return datetime.datetime.strptime(dstr, format_of_date_string)
 #########################################################################
 
-# TRANSFORM PRICE STRING INTO FLOAT ##########################################
-def price_as_float_NASDAQ(price):
-    ''' Method that makes sure price value is a float '''
-    # Removes the $ sign from the string
-    if type(price) == str:
-        # Transform the string and return the corresponding float
-        price = price.lstrip('$')
-        return float(price)
-    elif type(price) == float:
-        # Price already a float, return it
-        return price
-    else:
-        print('Weird... price columns are neither string nor float... check source')
-###########################################################################
-
 # Calculate Change from Day to Day ########################################
-def add_change_column(df0):
-    # Column initiated to calculate the % Change column
-    changes_list = []
-    # Initiating previous close
-    previous_close = df0.iloc[0]['Close']
-    # Iterate through rows to calculate changes from day to day
-    for i, j in df0.iterrows():
-        day_close = j['Close']
-        day_change = ((day_close - previous_close) / previous_close) * 100
-        changes_list.append(day_change)
-        # Get ready for next iteration
-        previous_close = day_close
-    df0['Change'] = changes_list
+def generate_change_column(df0):
+    # Be n the length of the data frame
+    n = df0.__len__()
+    # Iterating over the data frame rows
+    for r in range(1, n):
+        previous_close = df0.iloc[r-1]['Close']
+        close = df0.iloc[r]['Close']
+        try:
+            change = ((close - previous_close) / previous_close)*100
+            df0.at[df0.index[r], 'Change'] = change
+        except ZeroDivisionError:
+            print('Previous Close = 0, Cannot calculate change. ZeroDivisionError.')
     return df0
 ##########################################################
 
@@ -219,11 +190,13 @@ def is_showing_rstrength(spy_change, stock_change, spy_large_move):
         return False
 #####################################################################
 
-# MAIN #######################################################
+# MAIN ################################################################################################3
 # Read and prepare data
-spy_df = read_nasdaq_csv(data_files_names[0])
-tsla_df = read_nasdaq_csv(data_files_names[1])
+spy_df = read_data_source_csv(data_files_names[0])
+tsla_df = read_data_source_csv(data_files_names[1])
 
+
+'''
 # Initial position taken - max possible with capital to invest
 initial_position_qty = int(starting_capital / tsla_df['Close'][0])
 
@@ -243,3 +216,5 @@ rs_df = add_strategy_columns(rs_df)
 # PLOT
 # Plot the selected columns
 rs_df.loc[ : , ['TSLA Buy and Hold Value', 'Total Equity'] ].plot()
+'''
+#######################################################################################################3
